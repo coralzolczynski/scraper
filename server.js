2e9e2e3ee6571267905fafb822650e7d01470945
unknown <cmzolczynski@gmail.com>
2019-07-24 22:23:40 -0700

    server routes/html

--- server.js
+++ server.js
@@ -68,3 +68,19 @@ app.get("/articles/:id", (req, res) => {
         });
 });
 
+app.post("/articles/:id", (req, res) => {
+    db.Note.create(req.body)
+        .then(function(dbNote) {
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { not: dbNote._id }, { new: true});
+        })
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+});
+
+app.listen(PORT, () => {
+    console.log("App running on port" + PORT + "!")
+});
